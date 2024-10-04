d33151a5a73ca3919a64b5ff339d66d64f3163e4
TheSteeNos <davidsteens2001@gmail.com>
2024-10-04 19:24:57 +0200

    Added Norwegian to the Terms List

--- terms.mjs
+++ terms.mjs
@@ -1,31 +1,63 @@
 const TERMS = {
-    DRAW: "It's a Draw!",
-    MENU: "MENU",
-    PLAY: "1. Play Game",
-    SETTINGS: "2. Settings",
-    EXIT: "3. Exit Game",
-    GAMEOVER: "GAME OVER",
-    SPACE: " ",
-    EMPTY: "",
-    PLAYERPROMPTS: {
-        PLACE: "Place your mark at: ",
-        TURN1: "Player ",
-        TURN2: ", it's your turn!"
+    EN: {
+        DRAW: "It's a Draw!",
+        MENU: "MENU",
+        PLAY: "1. Play Game",
+        SETTINGS: "2. Settings",
+        EXIT: "3. Exit Game",
+        GAMEOVER: "GAME OVER",
+        SPACE: " ",
+        EMPTY: "",
+        PLAYERPROMPTS: {
+            PLACE: "Place your mark at: ",
+            TURN1: "Player ",
+            TURN2: ", it's your turn!"
+        },
+        PLAYER: {
+            ONE: "1",
+            TWO: "2"
+        },
+        WINNER: {
+            PART1: "Winner: ",
+            PART2: "Player ",
+            PART3: "!",
+        },
+        MARKS: {
+            X: "X ",
+            O: "O ",
+            _: "_ "
+        }
     },
-    PLAYER: {
-        ONE: "1",
-        TWO: "2"
-    },
-    WINNER: {
-        PART1: "Winner: ",
-        PART2: "Player ",
-        PART3: "!",
-    },
-    MARKS: {
-        X: "X ",
-        O: "O ",
-        _: "_ "
-    }
+
+    NO: {
+        DRAW: "Uavgjort!",
+        MENU: "MENY",
+        PLAY: "1. Spill Spillet",
+        SETTINGS: "2. Instillinger",
+        EXIT: "3. Avslutt Spill",
+        GAMEOVER: "Spillet er Over!",
+        SPACE: " ",
+        EMPTY: "",
+        PLAYERPROMPTS: {
+            PLACE: "Plasser ditt merke ved: ",
+            TURN1: "Spiller ",
+            TURN2: ", det er din tur!"
+        },
+        PLAYER: {
+            ONE: "1",
+            TWO: "2"
+        },
+        WINNER: {
+            PART1: "Vinner: ",
+            PART2: "Spiller ",
+            PART3: "!",
+        },
+        MARKS: {
+            X: "X ",
+            O: "O ",
+            _: "_ "
+        }
+    }  
 }
 
 export { TERMS }
