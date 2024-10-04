da79f7925a64c39505f480b00226b77d95b2df19
TheSteeNos <davidsteens2001@gmail.com>
2024-10-04 21:07:15 +0200

    Added to the Splash Art

--- splash.mjs
+++ splash.mjs
@@ -1,13 +1,13 @@
 import { ANSI } from "./ansi.mjs";
 
 const ART = `
-${ANSI.COLOR.RED} ______  ____   __   ${ANSI.COLOR.BLUE} ______   ____    __   ${ANSI.COLOR.YELLOW} ______   ___     ___
-${ANSI.COLOR.RED}|      ||    | /  ]  ${ANSI.COLOR.BLUE}|      | /    |  /  ]  ${ANSI.COLOR.YELLOW}|      | /   \\   /  _]
-${ANSI.COLOR.RED}|      | |  | /  /   ${ANSI.COLOR.BLUE}|      ||  o  | /  /   ${ANSI.COLOR.YELLOW}|      ||     | /  [_
-${ANSI.COLOR.RED}|_|  |_| |  |/  /    ${ANSI.COLOR.BLUE}|_|  |_||     |/  /    ${ANSI.COLOR.YELLOW}|_|  |_||  O  ||    _]
-${ANSI.COLOR.RED}  |  |   |  /   \\_     ${ANSI.COLOR.BLUE}|  |  |  _  /   \\_ ${ANSI.COLOR.YELLOW}    |  |  |     ||   [_
-${ANSI.COLOR.RED}  |  |   |  \\     |    ${ANSI.COLOR.BLUE}|  |  |  |  \\     |${ANSI.COLOR.YELLOW}    |  |  |     ||     |
-${ANSI.COLOR.RED}  |__|  |____\\____|    ${ANSI.COLOR.BLUE}|__|  |__|__|\\____|${ANSI.COLOR.YELLOW}    |__|   \\___/ |_____|
+${ANSI.COLOR.RED} ______  ____   __   ${ANSI.COLOR.GREEN} ______   ____    __   ${ANSI.COLOR.YELLOW} ______   ___     ___    ${ANSI.COLOR.BLUE} __
+${ANSI.COLOR.RED}|      ||    | /  ]  ${ANSI.COLOR.GREEN}|      | /    |  /  ]  ${ANSI.COLOR.YELLOW}|      | /   \\   /  _]  ${ANSI.COLOR.BLUE} |  |
+${ANSI.COLOR.RED}|      | |  | /  /   ${ANSI.COLOR.GREEN}|      ||  o  | /  /   ${ANSI.COLOR.YELLOW}|      ||     | /  [_    ${ANSI.COLOR.BLUE}|  |
+${ANSI.COLOR.RED}|_|  |_| |  |/  /    ${ANSI.COLOR.GREEN}|_|  |_||     |/  /    ${ANSI.COLOR.YELLOW}|_|  |_||  O  ||    _]   ${ANSI.COLOR.BLUE}|  |
+${ANSI.COLOR.RED}  |  |   |  /   \\_     ${ANSI.COLOR.GREEN}|  |  |  _  /   \\_ ${ANSI.COLOR.YELLOW}    |  |  |     ||   [_  ${ANSI.COLOR.BLUE}  |__|   
+${ANSI.COLOR.RED}  |  |   |  \\     |    ${ANSI.COLOR.GREEN}|  |  |  |  \\     |${ANSI.COLOR.YELLOW}    |  |  |     ||     | ${ANSI.COLOR.BLUE}   __
+${ANSI.COLOR.RED}  |__|  |____\\____|    ${ANSI.COLOR.GREEN}|__|  |__|__|\\____|${ANSI.COLOR.YELLOW}    |__|   \\___/ |_____|${ANSI.COLOR.BLUE}   |__|
 ${ANSI.RESET}
 `;
 
