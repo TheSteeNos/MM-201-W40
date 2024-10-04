142d4d7a1881487545a5fa46e6f79969993745a6
TheSteeNos <davidsteens2001@gmail.com>
2024-10-04 19:13:05 +0200

    Coloured Splash Screen

--- splash.mjs
+++ splash.mjs
@@ -1,14 +1,15 @@
+import { ANSI } from "./ansi.mjs";
 
 const ART = `
- ______  ____   __      ______   ____    __      ______   ___     ___
-|      ||    | /  ]    |      | /    |  /  ]    |      | /   \\   /  _]
-|      | |  | /  /     |      ||  o  | /  /     |      ||     | /  [_
-|_|  |_| |  |/  /      |_|  |_||     |/  /      |_|  |_||  O  ||    _]
-  |  |   |  /   \\_       |  |  |  _  /   \\_       |  |  |     ||   [_
-  |  |   |  \\     |      |  |  |  |  \\     |      |  |  |     ||     |
-  |__|  |____\\____|      |__|  |__|__|\\____|      |__|   \\___/ |_____|
-
-`
+${ANSI.COLOR.RED} ______  ____   __     ${ANSI.COLOR.BLUE} ______   ____    __    ${ANSI.COLOR.YELLOW}  ______   ___     ___
+${ANSI.COLOR.RED}|      ||    | /  ]    ${ANSI.COLOR.BLUE}|      | /    |  /  ]   ${ANSI.COLOR.YELLOW} |      | /   \\   /  _]
+${ANSI.COLOR.RED}|      | |  | /  /     ${ANSI.COLOR.BLUE}|      ||  o  | /  /    ${ANSI.COLOR.YELLOW} |      ||     | /  [_
+${ANSI.COLOR.RED}|_|  |_| |  |/  /      ${ANSI.COLOR.BLUE}|_|  |_||     |/  /     ${ANSI.COLOR.YELLOW} |_|  |_||  O  ||    _]
+${ANSI.COLOR.RED}  |  |   |  /   \\_       ${ANSI.COLOR.BLUE}|  |  |  _  /   \\_  ${ANSI.COLOR.YELLOW}     |  |  |     ||   [_
+${ANSI.COLOR.RED}  |  |   |  \\     |      ${ANSI.COLOR.BLUE}|  |  |  |  \\     | ${ANSI.COLOR.YELLOW}     |  |  |     ||     |
+${ANSI.COLOR.RED}  |__|  |____\\____|      ${ANSI.COLOR.BLUE}|__|  |__|__|\\____| ${ANSI.COLOR.YELLOW}     |__|   \\___/ |_____|
+${ANSI.RESET}
+`;
 
 function showSplashScreen() {
     console.log(ART);
