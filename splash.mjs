33296ec7faa79d220b56ea2320427385e200b805
TheSteeNos <davidsteens2001@gmail.com>
2024-10-04 20:55:38 +0200

    Updated Splash Art

--- splash.mjs
+++ splash.mjs
@@ -1,13 +1,13 @@
 import { ANSI } from "./ansi.mjs";
 
 const ART = `
-${ANSI.COLOR.RED} ______  ____   __     ${ANSI.COLOR.BLUE} ______   ____    __    ${ANSI.COLOR.YELLOW}  ______   ___     ___
-${ANSI.COLOR.RED}|      ||    | /  ]    ${ANSI.COLOR.BLUE}|      | /    |  /  ]   ${ANSI.COLOR.YELLOW} |      | /   \\   /  _]
-${ANSI.COLOR.RED}|      | |  | /  /     ${ANSI.COLOR.BLUE}|      ||  o  | /  /    ${ANSI.COLOR.YELLOW} |      ||     | /  [_
-${ANSI.COLOR.RED}|_|  |_| |  |/  /      ${ANSI.COLOR.BLUE}|_|  |_||     |/  /     ${ANSI.COLOR.YELLOW} |_|  |_||  O  ||    _]
-${ANSI.COLOR.RED}  |  |   |  /   \\_       ${ANSI.COLOR.BLUE}|  |  |  _  /   \\_  ${ANSI.COLOR.YELLOW}     |  |  |     ||   [_
-${ANSI.COLOR.RED}  |  |   |  \\     |      ${ANSI.COLOR.BLUE}|  |  |  |  \\     | ${ANSI.COLOR.YELLOW}     |  |  |     ||     |
-${ANSI.COLOR.RED}  |__|  |____\\____|      ${ANSI.COLOR.BLUE}|__|  |__|__|\\____| ${ANSI.COLOR.YELLOW}     |__|   \\___/ |_____|
+${ANSI.COLOR.RED} ______  ____   __   ${ANSI.COLOR.BLUE} ______   ____    __   ${ANSI.COLOR.YELLOW} ______   ___     ___
+${ANSI.COLOR.RED}|      ||    | /  ]  ${ANSI.COLOR.BLUE}|      | /    |  /  ]  ${ANSI.COLOR.YELLOW}|      | /   \\   /  _]
+${ANSI.COLOR.RED}|      | |  | /  /   ${ANSI.COLOR.BLUE}|      ||  o  | /  /   ${ANSI.COLOR.YELLOW}|      ||     | /  [_
+${ANSI.COLOR.RED}|_|  |_| |  |/  /    ${ANSI.COLOR.BLUE}|_|  |_||     |/  /    ${ANSI.COLOR.YELLOW}|_|  |_||  O  ||    _]
+${ANSI.COLOR.RED}  |  |   |  /   \\_     ${ANSI.COLOR.BLUE}|  |  |  _  /   \\_ ${ANSI.COLOR.YELLOW}    |  |  |     ||   [_
+${ANSI.COLOR.RED}  |  |   |  \\     |    ${ANSI.COLOR.BLUE}|  |  |  |  \\     |${ANSI.COLOR.YELLOW}    |  |  |     ||     |
+${ANSI.COLOR.RED}  |__|  |____\\____|    ${ANSI.COLOR.BLUE}|__|  |__|__|\\____|${ANSI.COLOR.YELLOW}    |__|   \\___/ |_____|
 ${ANSI.RESET}
 `;
 
